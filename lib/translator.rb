d857d83a6b865c4acf13bc99cf37927e3ab6f942
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 16:53:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,12 @@ def get_japanese_emoticon(file_path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-#def get_english_meaning
-   #code goes here
-#end
+def get_english_meaning(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoticons.each do |key, pairs|
+    if emoticon == pairs[:japanese]
+      return key
+    end
+  end
+  return "Sorrr, that emoticon was not found"  
+end
