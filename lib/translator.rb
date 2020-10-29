bfaa93bc85b749ab7845d53b9e684ca62f8efb8d
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 15:53:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,14 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file("./lib/emoticons.yml")
+  return emoticons
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
+  emoticons = YAML.load_file("./lib/emoticons.yml"
+  :emoticon[0] = :emoticon[1]
+  return :emoticon[0]
   # code goes here
 end
 
